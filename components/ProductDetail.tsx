1fa9e3be07a02e125d948966bb6ca073eb7bf51d
okware tom <okwaretom12@gmail.com>
2026-01-21 14:38:37 +0300

    Add icon to quality assurance badge

--- components/ProductDetail.tsx
+++ components/ProductDetail.tsx
@@ -655,7 +655,14 @@ export function ProductDetail({ slug }: ProductDetailProps) {
                            selectedUnitData.condition}
               </span>
               </div>
-              <span className="px-2 py-1 bg-blue-50 text-blue-700 rounded border border-blue-200 text-xs font-semibold">
+              <span className="flex items-center gap-1 px-2 py-1 bg-blue-50 text-blue-700 rounded border border-blue-200 text-xs font-semibold">
+                <svg className="w-3 h-3" fill="currentColor" viewBox="0 0 20 20">
+                  <path
+                    fillRule="evenodd"
+                    d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
+                    clipRule="evenodd"
+                  />
+                </svg>
                 Quality assured product
               </span>
             </div>

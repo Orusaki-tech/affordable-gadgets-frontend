d5c85bf5b305bc85e7e6cfa3f5eea0db1b8438c7
okware tom <okwaretom12@gmail.com>
2026-03-04 15:37:54 +0300

    made changes to search icon

--- components/HomeHero.tsx
+++ components/HomeHero.tsx
@@ -181,8 +181,9 @@ export function HomeHero() {
                   inputMode="search"
                 />
                 <button className="home-hero__search-button" type="submit" aria-label="Search">
-                  <svg className="home-hero__search-icon" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 0 0114 0z" />
+                  <svg className="home-hero__search-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round">
+                    <circle cx="11" cy="11" r="7" />
+                    <line x1="16.5" y1="16.5" x2="21" y2="21" />
                   </svg>
                 </button>
               </div>
